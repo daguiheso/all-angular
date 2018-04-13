01fc4af8d37a932336e8d2b4e8397c67ac6c9d14
Daniel Hernández <daguiheso@gmail.com>
2018-04-12 20:18:01 -0500

    Simple autocomplete with custom filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import { startWith } from 'rxjs/operators/startWith';
+import { map } from 'rxjs/operators/map';
+
+const fruits = [
+  { id: 1, name: 'Pera' },
+  { id: 2, name: 'Manzana' },
+  { id: 3, name: 'Mandarina' },
+  { id: 4, name: 'Coco' },
+  { id: 5, name: 'Banano' },
+  { id: 6, name: 'Sandia' },
+  { id: 7, name: 'Melon' },
+  { id: 8, name: 'Papaya' },
+  { id: 9, name: 'Uva' },
+];
 
 @Component({
   selector: 'app-root',
@@ -6,5 +22,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'app';
+
+  myControl: FormControl = new FormControl();
+
+  fruits: any[];
+
+  filteredOptions: Observable<string[]>;
+
+  constructor() {
+    this.fruits = fruits;
+  }
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(val => this.filter(val))
+      );
+  }
+
+  filter(val: string): string[] {
+    return this.fruits.filter(fruit =>
+      fruit.name.toLowerCase().indexOf(val.toLowerCase()) === 0
+    );
+  }
 }
